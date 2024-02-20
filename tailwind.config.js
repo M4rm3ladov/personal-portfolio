2d00a41aa1e1914b7667cf64fb043ec272ed8861
M4rm3ladov <renier.antonio20@gmail.com>
2024-02-20 08:25:36 +0800

    resized images and hamburger buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+   // These paths are just examples, customize them to match your project structure
+  purge: [
+    './*.html',
+  ],
   content: ['./*.{html,js}'],
   darkMode: false,
   theme: {
